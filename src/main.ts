99fe42896d6d8b014aa4846ead0860a6d51416f9
feynmanxiong <463175047@qq.com>
2019-06-19 17:31:32 +0800

    core_4

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DemoFilter } from './core/filters/demo.filter';
+import { LoggingInterceptor } from 'dist/core/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalFilters(new DemoFilter());
+  // app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(5000);
 }
 bootstrap();
--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,22 +1,31 @@
-import { Controller, Get, Req, Query, Param, Body, Headers, Post, HttpException, HttpStatus, ForbiddenException, UseFilters, UsePipes, ValidationPipe, ParseIntPipe, UseGuards, SetMetadata } from '@nestjs/common';
+import { Controller, Get, Req, Query, Param, Body, Headers, Post, HttpException, HttpStatus, ForbiddenException, UseFilters, UsePipes, ValidationPipe, ParseIntPipe, UseGuards, SetMetadata, UseInterceptors } from '@nestjs/common';
 import {CreatePostDto} from './post.dto'
 import { DemoService } from './providers/demo/demo.service';
 import { DemoFilter } from '../../core/filters/demo.filter';
 import { DemoAuthGuard } from '../../core/guards/demo-auth.guard';
 import { Roles } from '../../core/decorators/roles.decorator';
+import { LoggingInterceptor } from '../../core/interceptors/logging.interceptor';
+import { TransformInterceptor } from '../../core/interceptors/transform.interceptor';
+import { ErrorsInterceptor } from '../../core/interceptors/errors.interceptor';
+import { User } from '../../core/decorators/user.decorator';
+import { DemoPipe } from '../../core/pipes/demo.pipe';
 
 @Controller('posts')
 // @UseGuards(DemoAuthGuard)
+// @UseInterceptors(LoggingInterceptor)
 export class PostsController {
     constructor(private readonly demoService: DemoService) {}
 
     @Get()
+    // @UseInterceptors(TransformInterceptor)
+    @UseInterceptors(ErrorsInterceptor)
     index() {
-       return this.demoService.findAll();
+        throw new ForbiddenException();
+    //    return this.demoService.findAll();
     }
 
     @Get(':id')
-    show(@Param('id', ParseIntPipe) id) {
+    show(@Param('id', ParseIntPipe, DemoPipe) id) {
         console.log('id:', typeof id);
         return {
             title: `Post ${id}`
@@ -28,7 +37,8 @@ export class PostsController {
     @UsePipes(ValidationPipe)
     // @SetMetadata('roles', ['member'])
     @Roles('member')
-    store(@Body() post: CreatePostDto) {
+    store(@Body() post: CreatePostDto, @User('demo') user) {
+        console.log(user);
         // throw new HttpException('没有权限', HttpStatus.FORBIDDEN)
         // throw  new ForbiddenException('没有权限')
         this.demoService.create(post);   
