080f2a50f51fdd4bf74545ffab248ea6a9883c4e
feynmanxiong <463175047@qq.com>
2019-06-20 21:05:51 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DemoFilter } from './core/filters/demo.filter';
-import { LoggingInterceptor } from 'dist/core/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalFilters(new DemoFilter());
-  // app.useGlobalInterceptors(new LoggingInterceptor());
-  await app.listen(5000);
+  await app.listen(3000);
 }
 bootstrap();
--- src/modules/posts/posts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PostsController } from './posts.controller';
-import { DemoService } from '../posts/providers/demo/demo.service';
-
-@Module({
-    controllers: [PostsController],
-    providers: [DemoService]
-})
-export class PostsModule {}
