391501516e384be041c6c382aaa9041b2d826e9a
feynmanxiong <463175047@qq.com>
2019-06-26 16:35:11 +0800

    : )

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -7,7 +7,7 @@ import { CategoryService } from './category.service';
 @Controller('categories')
 export class CategoryController {
     constructor(
-        private readonly categoryService: CategoryService;
+        private readonly categoryService: CategoryService
     ){}
 
     @Post()
